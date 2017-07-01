6d1c0f4363b41cb9563423137456bfbc2fc4243c
ios <surendrakumar@MacBook-Air.local>
2017-07-02 00:05:07 +0530

    Initial Commit

---
+++ GPS Address Book/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GPS Address Book
+//
+//  Created by surendra kumar on 7/2/17.
+//  Copyright © 2017 weza. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
